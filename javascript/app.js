97323e0e2e0395147623d9fd33a70f6c6df59fe2
Syed Musab <syedmasab125@gmail.com>
2024-08-04 00:52:56 -0700

    first class javacript done

---
+++ javascript/app.js
@@ -0,0 +1,57 @@
+// var value1 =  2; Number
+// var text = " hello world "  string
+// var text = " hello world"
+// console.log(text)
+
+// legal and illeagl
+// var setCount  camel  case
+//  illegal
+// var 2value
+// var set counbt
+// var $*_
+
+// math expreession
+// + - * / % > < === ==
+// var value1 = 5;
+// var value2 =  3;
+// var addition = value1 + value2;
+// var subtraction = value1 - value2;
+// var multipication =  value1 * value2
+// var divide =   value1 / value2
+// var modulus =  value1 % value2
+// console.log( addition , subtraction , multipication , divide , modulus  );
+// unfamiliar
+// var num1 = 5;
+// var num2 = 6;
+// reinitialize redeclare
+// num1 =  5 + 1;
+// post increment
+// var num1 = num1++;
+// pre increment
+// var num2 = ++num2;
+// Math ambiguity
+// var calculate = ((( ( 5 + 6 ) * 2 )+ 6) / 4) * 2
+// console.log(calculate);
+// Concatenation
+// var text = "hello";
+// var text2 = "wolrd"
+// console.log(text + text2 )
+
+// var num1 = "5";
+// var num2 = 6;
+// console.log(num1+num2)
+
+// conditional statement
+//  === type value , == value 
+var temp = 5;
+if (temp > 7) {
+  console.log("you should wear a jacket");
+}
+if (temp == "5" ) {
+  console.log(" you should wear double jacket");
+}
+ else {
+  console.log(" teampreture normnal ");
+}
+
+// console.log(num1,num2);
