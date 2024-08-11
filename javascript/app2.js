2a3047613c0a8a89abd2a839d0043bafd17a079b
Syed Musab <syedmasab125@gmail.com>
2024-08-11 00:57:34 -0700

    javascript2 done

---
+++ javascript/app2.js
@@ -0,0 +1,42 @@
+// else if 
+// var chotaBhai = 6
+// if(chotaBhai > 3){
+//     console.log("tum ghar pe hi rahogey ")
+// }else if(chotaBhai == 5  ){
+//     console.log("thik hai ro mat sath cahl saktey ho")
+
+// }else if(chotaBhai > 6 ){
+//     console.log("eidi tum share karogey")
+// }
+// else {
+//     console.log("ab kam nahi kar waq saktey ")
+// }
+// arrays
+// var student1 = "umaima"
+// var student2 = "hifza"
+// var student3 = "sadaf"
+// array indexing   0          1           2   3           4
+// var students = ["umaima","anoosha","unsa","arisha","javeria" ]
+// students.pop() delete last element
+// students.push("noor" , "abiha") 
+// students.shift()
+// students.unshift("eman")
+// students.splice(position, how many value delete , value add )
+// students.splice(2,2, "eman","azca")
+// var newStudents =  students.slice(1,4) index , position
+// console.log(newStudents)
+// students[2] = "sahar"
+//  loops for , while ,do while
+// console.log("2 x 1 = 2 ")
+// console.log("2 x 2 = 4 ")
+// console.log("2 x 3 = 6 ")
+
+// for(initilization, condition , increment){
+
+// }
+for(var i = 1 ; i < 11 ; i++ ){
+    console.log( 2 + "x" + i + "=" + 2*i )
+}
+
+
+
