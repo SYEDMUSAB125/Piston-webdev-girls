8eec3713678a434d5feb7408d8e2bef0d4897328
Syed Musab <syedmasab125@gmail.com>
2024-08-17 01:26:43 -0700

    javascript function done

---
+++ javascript/app3.js
@@ -0,0 +1,54 @@
+// var right = new Date()
+// // console.log(right)
+// var date = right.getDate()
+// var day = right.getDay()
+// var year = right.getFullYear()
+// console.log("date", date )
+// console.log("daY", day )
+// console.log("year", year )
+
+// Age calculator
+// function ageCalculator() {
+//   var rightNow = new Date();
+//   var currentYear = rightNow.getFullYear();
+//   var bornYear = Number(prompt("Enter your Born Year"));
+//   var age = currentYear - bornYear;
+//   console.log("MY AGE IS ", age);
+// }
+
+
+// function call 
+// ageCalculator()
+
+
+                    // paramter    
+function Greeting (name1,name2,name3,name4){
+    return(["Assalam u alaikum", name1 , name2, name3,name4 ] )
+}
+            // Argument
+var a = Greeting("Areeba","unmaima","fozia","noor")
+console.log(a)
+
+
+
+
+// quiz 
+// netflix
+// switch case
+// me shapat leti ho din me 5 video daikhogi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
